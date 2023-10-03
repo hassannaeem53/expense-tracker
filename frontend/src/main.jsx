b6a59dd302da8a91523457035a5f89bb939740db
hassannaeem53 <hassannaeem53@gmail.com>
2023-10-03 11:41:20 +0500

    Project Structure  Updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Provider } from 'react-redux'; // Import Provider
 import store from './redux/store'; // Import your Redux store
-import Dashboard from './components/Dashboard'; // Import the Dashboard component
+import App from './App';
 
 const darkTheme = createTheme({
   palette: {
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Dashboard />
+      <App />
     </ThemeProvider>
   </Provider>,
 );
