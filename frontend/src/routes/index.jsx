38dbe11cb5a6f626a3eec03849dc670a8a1fe4f7
Shaiq e Mustafa <shaiq@gosaas.io>
2023-10-03 12:35:32 +0500

    Added states and components for authentication

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import NotFoundPage from '../pages/NotFound';
 import Dashboard from '../components/Dashboard';
+import Auth from '../components/Auth';
 
 const AppRouter = () => (
   <Routes>
+    <Route path="/login" element={<Auth />} />
     <Route path="/dashboard" element={<Dashboard />} />
     <Route path="*" element={<NotFoundPage />} />
   </Routes>
